fc10bba3fcca4ec8445b94a2df13aa626c7a3fda
kosuda_tatsuya <tatsuya.kosuda@gmail.com>
2019-07-11 19:50:23 +0900

    first commit

---
+++ Assets/ThirdParty/Pcx/Editor/PlyImporterInspector.cs
@@ -0,0 +1,43 @@
+﻿// Pcx - Point cloud importer & renderer for Unity
+// https://github.com/keijiro/Pcx
+
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Experimental.AssetImporters;
+
+namespace Pcx
+{
+    // Note: Not sure why but EnumPopup doesn't work in ScriptedImporterEditor,
+    // so it has been replaced with a normal Popup control.
+
+    [CustomEditor(typeof(PlyImporter))]
+    class PlyImporterInspector : ScriptedImporterEditor
+    {
+        SerializedProperty _containerType;
+
+        SerializedProperty _offset;
+
+        string[] _containerTypeNames;
+
+        protected override bool useAssetDrawPreview { get { return false; } }
+
+        public override void OnEnable()
+        {
+            base.OnEnable();
+
+            _containerType = serializedObject.FindProperty("_containerType");
+            _containerTypeNames = System.Enum.GetNames(typeof(PlyImporter.ContainerType));
+            _offset = serializedObject.FindProperty("_offset");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            _containerType.intValue = EditorGUILayout.Popup(
+                "Container Type", _containerType.intValue, _containerTypeNames);
+
+            _offset.vector3Value = EditorGUILayout.Vector3Field("offset", _offset.vector3Value);
+
+            base.ApplyRevertGUI();
+        }
+    }
+}
